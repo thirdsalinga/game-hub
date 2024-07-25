ba8275f73cfe9e156fc43ef6ed37044865f6d9cf
thirdsalinga <48078466+thirdsalinga@users.noreply.github.com>
2024-07-25 18:07:21 +0800

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card width={'300px'}>
       <Image
         src={getCroppedImageUrl(game.background_image)}
         borderRadius={10}
