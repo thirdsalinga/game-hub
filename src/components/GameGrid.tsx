0b7b7815ddfa520fdd8e705ce4d3ad691020ef83
thirdsalinga <48078466+thirdsalinga@users.noreply.github.com>
2024-07-25 17:49:03 +0800

    Displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,11 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid padding='10px' columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+      <SimpleGrid
+        padding="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+      >
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
